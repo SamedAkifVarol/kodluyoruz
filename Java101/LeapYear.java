a61b81a1017b30bf7bda538b068aa7adf82a702c
SamedAkifVarol <samedakifvarol@gmail.com>
2022-05-19 12:59:55 +0300

    adding leap year

---
+++ Java101/LeapYear.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+
+        System.out.println("Pleaser enter year that you want : ");
+        year = sc.nextInt();
+
+        if(year % 4 == 0 && year % 400 == 0) {
+            System.out.println(year + " is leap year");
+        }
+        else {
+            System.out.println(year + " is not leap year");
+        }
+    }
+}
