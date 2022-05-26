ca4df6200e33d8cd6de7813633bfa98b2c3167d3
SamedAkifVarol <samedakifvarol@gmail.com>
2022-05-26 10:48:16 +0300

    adding frekans

---
+++ Java101/frekans.java
@@ -0,0 +1,9 @@
+public class frekans {
+    public static void main(String[] args) {
+        int[] dizi = {10,10,10,20,20,30,30,30,30};
+        int[] frekans = new int[dizi.length];
+        for(int i = 0;i < dizi.length;i++){
+            frekans[i] = 1;
+        }
+    }
+}
